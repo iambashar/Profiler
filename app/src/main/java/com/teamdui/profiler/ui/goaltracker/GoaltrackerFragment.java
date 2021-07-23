be5197aa133c552669022f382624f83b7d9e40fb
SamiaShashmi <shoshmiislam149@gmail.com>
2021-07-23 19:33:42 +0600

    goal tracker functionality done

--- app/src/main/java/com/teamdui/profiler/ui/goaltracker/GoaltrackerFragment.java
+++ app/src/main/java/com/teamdui/profiler/ui/goaltracker/GoaltrackerFragment.java
@@ -23,6 +23,12 @@ public class GoaltrackerFragment extends Fragment {
     private GoaltrackerViewModel goaltrackerViewModel;
     private FragmentGoaltrackerBinding binding;
     public Button goalButton;
+    public GoalsaveFragment goalsaveFragment;
+
+    public static int calorieGoal = 0;
+    public static int waterGoal = 0;
+    public static  int exerciseGoal = 0;
+    public static boolean isGoslsaveVisited = false;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -32,11 +38,16 @@ public class GoaltrackerFragment extends Fragment {
         binding = FragmentGoaltrackerBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+
+        setGoalVariables();
+        setGoalText();
+
         goalButton = binding.setGoalButton;
         goalButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Bundle arg = new Bundle();
+                isGoslsaveVisited = true;
                 Navigation.findNavController(root).navigate(R.id.action_navigation_goaltracker_to_navigation_goalsave, arg);
             }
         });
@@ -48,4 +59,24 @@ public class GoaltrackerFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    public void setGoalVariables()
+    {
+        if(isGoslsaveVisited)
+        {
+            calorieGoal = goalsaveFragment.getCalorieGoal();
+            waterGoal = goalsaveFragment.getGlassGoal();
+            exerciseGoal = goalsaveFragment.getExerciseGoal();
+        }
+    }
+    public void setGoalText()
+    {
+        String calorieText = binding.calorieEarninTracker.getText().toString();
+        binding.calorieEarninTracker.setText(calorieText + String.valueOf(calorieGoal));
+        String waterText = binding.waterTakeninTracker.getText().toString();
+        binding.waterTakeninTracker.setText(waterText + String.valueOf(waterGoal));
+        String exerciseText = binding.exerciseDoneinTracker.getText().toString();
+        binding.exerciseDoneinTracker.setText(exerciseText + String.valueOf(exerciseGoal));
+    }
+
 }
