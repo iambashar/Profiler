4d808b0206b54b8d863918d62200d9bf78976bd7
spiritinchains <22220607+spiritinchains@users.noreply.github.com>
2021-10-16 12:33:34 +0600

    Add help text

--- app/src/main/java/com/teamdui/profiler/ui/Help/HelpViewModel.java
+++ app/src/main/java/com/teamdui/profiler/ui/Help/HelpViewModel.java
@@ -8,8 +8,13 @@ public class HelpViewModel extends ViewModel {
     private MutableLiveData<String> mText;
     // TODO: Implement the ViewModel
     public  HelpViewModel() {
+        String helpText = "Use the in-app camera to take photos of food items to calculate their " +
+                "calorie content. Set goals for daily calorie consumption and exercise from the " +
+                "Goal Tracker, and view progress towards said goals.\n" +
+                "Meals can be added manually from the Daily Calorie view, additionally BMI can be " +
+                "calculated from the BMI calculator";
         mText = new MutableLiveData<>();
-        mText.setValue("This is Help fragment");
+        mText.setValue(helpText);
     }
     public LiveData<String> getText() {
         return mText;
